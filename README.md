fe1f89e308d4ad9f0d61ecacbb87aa63d026a039
0x800f081f <boehler.s@bluewin.ch>
2019-05-22 21:33:36 +0200

    first changes to the broker-service

--- README.md
+++ README.md
@@ -5,13 +5,6 @@
 TermTalk is a Java Application, which allows you to communicate with your friends inside your local network.
 It was written as a school-project for the IT-Module 226B.
 
-## Usage
-```
-cd finish/back-end
-mvn install
-mvn liberty:start-server
-```
-
 ## How it works
 ![](mdwiki/subChapter/images/howitworks.png)
 
@@ -23,3 +16,18 @@ U2 is used as key to get the IP out of the Hashmap.
 2. The Broker-Server responses the IP of user2 (in this case 192.168.1.2)
 
 3. User1 is connecting to the IP of User2 to communicate via peer-to-peer.
+
+## Server
+
+### Usage
+```
+cd finish/back-end
+mvn install
+mvn liberty:start-server
+```
+The broker-service is reachable under the following link: ![](http://localhost:9000/).
+TermTalk works with the JSON data-interchange format. Data can be gotten from the broker-server under the link ![](http://localhost:9000/broker/api/user).
+
+The Ports are as followed:
+  * HTTP: 9000
+  * HTTPS: deactivated at the moment due to complicity
--- finish/back-end/src/main/java/inauen/tutorial/rest/TeacherResource.java
+++ finish/back-end/src/main/java/inauen/tutorial/rest/TeacherResource.java
@@ -16,17 +16,17 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 @RequestScoped
-@Path("teachers")
+@Path("user")
 public class TeacherResource {
-	
+
 	/**
 	 * Der TeacherManager kuemmert Sich um die InMemoryStore. Er kommt nur einmal vor
 	 */
-	
+
 	@Inject
 	private TeacherManager teacherManager;
 
-	
+
 	/**
 	 * REST-API für HTTP-GET mit Parameter. Liefert einen Lehrer zurueck - benoetigt den Aufruf /ID
 	 * @param id
@@ -36,19 +36,19 @@ public class TeacherResource {
 	@Path( "/{id}" )
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getByID(@PathParam("id") String id) {
-		
+
 		Teacher teacher = teacherManager.get(id);
-		
+
 		if (teacher != null)
 		{
-			return Response.ok(teacher).build();	
+			return Response.ok(teacher).build();
 		}
-		else 
+		else
 		{
 			return Response.status(Status.NOT_FOUND).build();
 		}
 	}
-	
+
 	/**
 	 * REST-API fuer HTTP-GET ohne Parameter - soll die gesamte Liste an Teacher zurueckgeben
 	 * @return Liste aller Teacher
@@ -59,7 +59,7 @@ public class TeacherResource {
 	public Response getAll() {
 		return Response.ok(teacherManager.getAll()).build();
 	}
-	
+
 	/**
 	 * REST-API fuer HTTP-POST. Erwartet ein JSON-Objekt im POST drin. Erstellt einen neuen Lehrer
 	 * @param teacherJSON
@@ -70,20 +70,20 @@ public class TeacherResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response add(JsonObject teacherJSON)
-	{	
+	{
 		// Wir holen zuerst aus dem JSON unsere Werte
 		String vornamen = teacherJSON.getString(Teacher.ATTR_VORNAME);
 		String nachname = teacherJSON.getString(Teacher.ATTR_NACHNAME);
 		String fachbereich = teacherJSON.getString(Teacher.ATTR_FACHBEREICH);
-		
-		
-		
-		
+
+
+
+
 		Teacher newTeacher = new Teacher(vornamen, nachname, fachbereich);
 		String id = teacherManager.add(newTeacher);
-		return Response.ok(id).build();	
+		return Response.ok(id).build();
 	}
-	
+
 	/**
 	 * Loescht Teacherobjekt aufgrund der ID
 	 * @param id
@@ -95,7 +95,7 @@ public class TeacherResource {
 	public Response delete(@PathParam("id") String id)
 	{
 		Teacher teachObjekt = teacherManager.remove(id);
-		
+
 		if (teachObjekt != null)
 		{
 			return Response.ok(teachObjekt).build();
@@ -105,25 +105,25 @@ public class TeacherResource {
 			return Response.status(Status.NOT_FOUND).build();
 		}
 	}
-	
+
 	/**
 	 * REST-API auf PUT-Methode. Teacherobjekt (referenziert ueber die ID) wird angepasst
 	 * @param id
 	 * @param teacherJSON
 	 * @return Teacherobjekt oder 404
 	 */
-	
+
 	@PUT
 	@Path( "/{id}" )
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response put(@PathParam("id") String id, JsonObject teacherJSON)
-	{	
+	{
 		// Wir holen zuerst aus dem JSON unsere Werte
 		String vornamen = teacherJSON.getString(Teacher.ATTR_VORNAME);
 		String nachname = teacherJSON.getString(Teacher.ATTR_NACHNAME);
 		String fachbereich = teacherJSON.getString(Teacher.ATTR_FACHBEREICH);
-		
+
 		Teacher changed = teacherManager.merge(id, vornamen, nachname, fachbereich);
 		if (changed != null)
 		{
@@ -133,8 +133,8 @@ public class TeacherResource {
 		{
 			return Response.status(Status.NOT_FOUND).build();
 		}
-			
-	}	
-	
-	
+
+	}
+
+
 }
